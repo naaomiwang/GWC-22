b7ff1805ed737db8440968c669a415b92613eae2
Naomi Wang <94681116+naaomiwang@users.noreply.github.com>
2022-07-06 20:08:12 +0800

    Update script.js

--- PersonalityQuiz/script.js
+++ PersonalityQuiz/script.js
@@ -10,6 +10,7 @@ var q2a2 = document.getElementById("q2a2");
 var q3a1 = document.getElementById("q3a1"); 
 var q3a2 = document.getElementById("q3a2"); 
 var restartButton = document.getElementById("restartButton"); 
+var display = document.getElementById("display")
 
 q1a1.addEventListener('click',oldie); 
 q1a2.addEventListener('click',newbie); 
@@ -18,6 +19,7 @@ q2a2.addEventListener('click',oldie);
 q3a1.addEventListener('click',newbie); 
 q3a2.addEventListener('click',oldie); 
 restartButton.addEventListener('click',restart); 
+display.addEventListener('click',displayResult)
 
 
 function oldie(){
@@ -44,7 +46,7 @@ function newbie() {
   }
 }
 // Update quiz result 
-function updateResult() {
+/* function updateResult() {
   if (oldieScore >= 2){
     result.innerHTML = "You are an oldie!"
     console.log("You are an oldie!")
@@ -53,10 +55,60 @@ function updateResult() {
     console.log("You are a newbie!")
   }
 }
+*/
 
 function restart(){
   document.getElementById('result').innerHTML = "Your result is ..."
   var oldieScore = 0; 
   var newbieScore = 0; 
   var questionCount = 0;
+  enableQuestions();
 }
+
+// Manually display result 
+function displayResult(){
+  if (oldieScore >= 2){
+    result.innerHTML = "You are an oldie!"
+    console.log("You are an oldie!")
+  } else if (newbieScore >= 2) {
+    result.innerHTML = "You are a newbie!"
+    console.log("You are a newbie!")
+  } 
+}
+
+// Figure out how to disable the buttons once the question is answered. 
+
+q1a1.addEventListener("click", disableQ1);
+q1a2.addEventListener("click", disableQ1);
+
+q2a1.addEventListener("click", disableQ2);
+q2a2.addEventListener("click", disableQ2);
+
+q3a1.addEventListener("click", disableQ3);
+q3a2.addEventListener("click", disableQ3);
+
+function disableQ1() {
+  q1a1.disabled = true; 
+  q1a2.disabled = true; 
+}
+
+function disableQ2() {
+  q2a1.disabled = true; 
+  q2a2.disabled = true; 
+}
+
+function disableQ3() {
+  q3a1.disabled = true; 
+  q3a2.disabled = true; 
+}
+
+
+function enableQuestions(){
+  q1a1.disabled = false; 
+  q1a2.disabled = false; 
+  q2a1.disabled = false; 
+  q2a2.disabled = false;
+  q3a1.disabled = false; 
+  q3a2.disabled = false;
+}
+
